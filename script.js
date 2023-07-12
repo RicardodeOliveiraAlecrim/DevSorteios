4aa515ce0224c2982ee320521f15c6fcdffea930
RicardodeOliveiraAlecrim <134964500+RicardodeOliveiraAlecrim@users.noreply.github.com>
2023-07-12 15:07:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function generateNumber() {
+      
+  const min = Math.ceil(document.querySelector(".min").value)
+  const max = Math.floor(document.querySelector(".max").value)
+
+  const results = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    
+  alert(results)
+    
+}
