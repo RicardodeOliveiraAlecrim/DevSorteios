6921821190303b99082f3cde6232c76ec8f64125
RicardodeOliveiraAlecrim <134964500+RicardodeOliveiraAlecrim@users.noreply.github.com>
2023-07-13 12:18:29 +0000

    edição de alert

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function generateNumbers() {
-      
+
+  
   const min = Math.ceil(document.querySelector(".min").value)
   const max = Math.floor(document.querySelector(".max").value)
+  
+  if (min >= max) {
+    alert("O VALOR MÍNIMO DEVE SER MENOR QUE O VALOR MÁXIMO")
+  } else {
+    
 
   const results = Math.floor(Math.random() * (max - min + 1)) + min;
 
     
   alert(results)
-    
+    }
 }
